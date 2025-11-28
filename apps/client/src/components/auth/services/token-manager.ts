15e089293d456b4c79ca9b8899b3878c1e773c4c
Hery Fitiavana <hheryfitiavana@gmail.com>
2025-11-28 17:59:20 +0300

    try fix refresh token by deleting code manullay

--- apps/client/src/components/auth/services/token-manager.ts
+++ apps/client/src/components/auth/services/token-manager.ts
@@ -1,32 +1,22 @@
 import { authService } from "./auth-service";
 
 type SessionResult = Awaited<ReturnType<typeof authService.getSession>>;
-type SessionPromise = Awaited<typeof authService.getSession>;
-type RefreshSessionPromise = Awaited<typeof authService.refreshSession>;
+type SessionPromise = ReturnType<typeof authService.getSession>;
 
 class TokenManager {
   private sessionResult: SessionResult | null = null;
   private getSessionPromise: SessionPromise | null = null;
-  private refreshPromise: RefreshSessionPromise | null = null;
 
   private async fetchSessionOnce() {
     if (this.getSessionPromise) return this.getSessionPromise;
-    this.getSessionPromise = authService.getSession;
+    this.getSessionPromise = authService.getSession();
     return this.getSessionPromise;
-  }
-
-  private async fetchRefreshOnce() {
-    if (this.refreshPromise) return this.refreshPromise;
-    this.refreshPromise = authService.refreshSession;
-    return this.refreshPromise;
   }
 
   async getToken() {
     if (this.sessionResult) return this.sessionResult;
 
-    const sessionPromiseCache = await this.fetchSessionOnce();
-    const sessionResult = await sessionPromiseCache();
-
+    const sessionResult = await this.fetchSessionOnce();
     if (!sessionResult.success) return sessionResult;
     if (!sessionResult.data?.accessToken)
       return {
@@ -37,29 +27,6 @@ class TokenManager {
 
     this.sessionResult = sessionResult;
 
-    //  Vérifier si le token n'est pas expiré
-    if (sessionResult.data.expiresAt) {
-      const expirationTime = sessionResult.data.expiresAt * 1000; // Convertir en millisecondes
-      const currentTime = Date.now();
-      const bufferTime = 5 * 60 * 1000; // 5 minutes de marge
-
-      if (currentTime >= expirationTime - bufferTime) {
-        console.warn("⚠️ Token expiré ou proche de l'expiration");
-        const refreshSessionCache = await tokenManager.refreshToken();
-        const refreshResult = await refreshSessionCache();
-        if (!refreshResult) return refreshResult;
-        if (!refreshResult.data?.accessToken)
-          return {
-            success: false,
-            error: "No access token found",
-            status: 404,
-          } as const;
-
-        this.sessionResult = refreshResult;
-        return refreshResult;
-      }
-    }
-
     return {
       success: true,
       data: {
@@ -67,11 +34,6 @@ class TokenManager {
         expiresAt: sessionResult.data.expiresAt,
       },
     } as const;
-  }
-
-  async refreshToken() {
-    const res = await this.fetchRefreshOnce();
-    return res;
   }
 
   async tokenInHeader() {
@@ -89,7 +51,6 @@ class TokenManager {
   clearCache() {
     this.sessionResult = null;
     this.getSessionPromise = null;
-    this.refreshPromise = null;
   }
 }
 
